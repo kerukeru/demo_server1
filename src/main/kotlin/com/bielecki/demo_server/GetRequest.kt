f570e8bf3f9cf016aa603a75674027cdb4005310
kerukeru <keru@protonmail.com>
2020-08-25 21:19:37 +0200

    Initial commit

---
+++ src/main/kotlin/com/bielecki/demo_server/GetRequest.kt
@@ -0,0 +1,42 @@
+package com.bielecki.demo_server
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+
+class GetRequest {
+
+fun myrequest(){
+    val urlForGetRequest = URL("http://localhost:8080/greeting")
+
+    var readLine: String? = null
+
+    val conection: HttpURLConnection = urlForGetRequest.openConnection() as HttpURLConnection
+
+    conection.requestMethod = "GET"
+
+    conection.setRequestProperty("userId", "a1bcdef") // set userId its a sample here
+
+
+    val responseCode: Int = conection.responseCode
+
+    if (responseCode == HttpURLConnection.HTTP_OK) {
+        val `in` = BufferedReader(
+                InputStreamReader(conection.inputStream))
+        val response = StringBuffer()
+        while (`in`.readLine().also { readLine = it } != null) {
+            response.append(readLine)
+        }
+        `in`.close()
+
+        // print result
+        println("JSON String Result $response")
+
+        //GetAndPost.POSTRequest(response.toString());
+    } else {
+        println("GET NOT WORKED")
+    }
+}
+}
